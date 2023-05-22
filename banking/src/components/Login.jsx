73f2ec6aab6550ce65da940fc2644e2e0bb0f8d3
iannnna <julianna.barrera03@gmail.com>
2023-05-22 17:30:00 +0800

    login-signup-page

---
+++ banking/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import './Login.css'
+
+const Login = () => {
+    return(
+        <div className='login-main-page'>
+            <div className='login-form-container'>
+                <h2>Login</h2>
+                <div className='login-form-container'>
+                    <form action=""></form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
